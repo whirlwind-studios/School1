b3ad74c8521bc75a8f2263943cd563e1e9fae73e
Leon Höpfl <leon.hoepfl.p@gmail.com>
2017-07-08 15:26:11 +0200

    Added basic Credentials class

--- Apps/Android/app/src/main/java/whirlwind/com/school1/MainActivity.java
+++ Apps/Android/app/src/main/java/whirlwind/com/school1/MainActivity.java
@@ -3,11 +3,20 @@ package whirlwind.com.school1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import whirlwind.com.school1.Backend.Credentials;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(!Credentials.isSignedIn())
+            Credentials.autoSignin(this);
     }
 }
